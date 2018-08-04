a932abf221bc0aa3953004bd665612dc5f3236c7
yumikg <yumiikegaya0528@gmail.com>
2018-08-04 09:03:04 +0900

    画面遷移：サブからメインの画面に戻る動きを追加

--- app/src/main/java/com/example/yumi/birthdayapplication/MainActivity.java
+++ app/src/main/java/com/example/yumi/birthdayapplication/MainActivity.java
@@ -41,43 +41,15 @@ public class MainActivity extends Activity
 
     //画面遷移
     public void onClick(View view) {
-        Intent intent = new Intent(this, SubActivity.class);
-        startActivity(intent);
-    }
 
+        Calendar birth = getBirthDay();
 
-    //////ここから誕生日までの日数計算
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
 
-    //リーダ
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    //フォーマッタ
-    private static DateFormat formatYYYYMD = new SimpleDateFormat("yyyy年M月d日");
-    private static DateFormat formatMMDD = new SimpleDateFormat("MMdd");
-
-
-    public static void main(String[]args) {
-
-        //当日の日付
-        Calendar cal = createCalendar();
-        System.out.print("今日は");
-        System.out.println(formatYYYYMD.format(cal.getTime())+"です");
-        System.out.println("あなたの次の誕生日までの日数は・・・");
-
-        // 誕生日を入力
-        Calendar birth = null;
-        try {
-            while (birth == null) {
-                birth = getBirthDay();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
 
         if (birth != null) {
             // 日数を計算するため、誕生日の年をシステム日付と合わせる
@@ -91,8 +63,62 @@ public class MainActivity extends Activity
             // 誕生日までの日数を計算
             long diff = (birth.getTimeInMillis() - cal.getTimeInMillis()) / 1000 / 60 / 60 / 24;
             System.out.println("誕生日まであと" + diff + "日です");
+
+
+            Intent intent = new Intent(this, SubActivity.class);
+            intent.putExtra("message", "誕生日まであと" + diff + "日です");
+            startActivity(intent);
         }
     }
+
+
+    //////ここから誕生日までの日数計算
+
+    //リーダ
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //フォーマッタ
+    private static DateFormat formatYYYYMD = new SimpleDateFormat("yyyy年M月d日");
+    private static DateFormat formatMMDD = new SimpleDateFormat("MMdd");
+
+
+//    public  void main(String[]args) {
+//
+//        //当日の日付
+//        Calendar cal = createCalendar();
+//        System.out.print("今日は");
+//        System.out.println(formatYYYYMD.format(cal.getTime())+"です");
+//        System.out.println("あなたの次の誕生日までの日数は・・・");
+//
+//        // 誕生日を入力
+//        Calendar birth = null;
+//        try {
+//            while (birth == null) {
+//                birth = getBirthDay();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                br.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        if (birth != null) {
+//            // 日数を計算するため、誕生日の年をシステム日付と合わせる
+//            birth.set(Calendar.YEAR, cal.get(Calendar.YEAR));
+//
+//            // 今年の誕生日が過ぎている場合は、誕生日を来年に設定
+//            if (birth.before(cal)) {
+//                birth.add(Calendar.YEAR, 1);
+//            }
+//
+//            // 誕生日までの日数を計算
+//            long diff = (birth.getTimeInMillis() - cal.getTimeInMillis()) / 1000 / 60 / 60 / 24;
+//            System.out.println("誕生日まであと" + diff + "日です");
+//        }
+//    }
 
     /**
      *   カレンダーインスタンス取得メソッド。 日数計算を行いやすくするため、時刻は全て00:00:00.000にセットする。
@@ -114,12 +140,14 @@ public class MainActivity extends Activity
      * @return 誕生日のカレンダーインスタンス
      * @throws IOException
      */
-    private static Calendar getBirthDay() throws IOException {
+    private Calendar getBirthDay() {
         System.out.println("誕生日を数字で入力して下さい（例：4月30日なら0430）");
         Calendar birth = null;
 
+        EditText birthday = (EditText) findViewById(R.id.editText3);
+
         try {
-            String mmdd = br.readLine();
+            String mmdd = birthday.getText().toString();
             Date b = formatMMDD.parse(mmdd);
             birth = createCalendar();
             birth.setTime(b);
--- app/src/main/java/com/example/yumi/birthdayapplication/SubActivity.java
+++ app/src/main/java/com/example/yumi/birthdayapplication/SubActivity.java
@@ -1,16 +1,33 @@
 package com.example.yumi.birthdayapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SubActivity extends Activity {
+public class SubActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        findViewById(R.id.buttonToMain).setOnClickListener(this);//画面遷移
+
+        Intent intent = getIntent();
+        String message = intent.getExtras().getString("message");
+        TextView birthday = (TextView) findViewById(R.id.textView2);
+        birthday.setText(message);
+
+
+    }
+
+    public void onClick(View view) {
+
+        finish();
 
     }
 }
